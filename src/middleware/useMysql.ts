f887c43596cd00b9421b1b880ac1df429e20b6fa
JunSeok <ppu1234@naver.com>
2022-07-23 22:12:26 +0900

    0723_commit

--- src/middleware/useMysql.ts
+++ src/middleware/useMysql.ts
@@ -1,22 +1,18 @@
-import { Request, Response, NextFunction } from "express"
-import mysql from "../modules/mysql"
+import { Request, Response, NextFunction } from 'express'
+import mysql from '../modules/mysql'
 
 interface RequestWithConnection extends Request {
-  mysqlConnection?: any
+    mysqlConnection?: any
 }
 
-export const useMysql = async (
-  req: RequestWithConnection,
-  res: Response,
-  next: NextFunction
-) => {
-  mysql
-    .connect()
-    .then((connection) => {
-      req.mysqlConnection = connection
-      next()
-    })
-    .catch((e: Error) => {
-      next(e)
-    })
+export const useMysql = async (req: RequestWithConnection, res: Response, next: NextFunction) => {
+    mysql
+        .connect()
+        .then((connection) => {
+            req.mysqlConnection = connection
+            next()
+        })
+        .catch((e: Error) => {
+            next(e)
+        })
 }
--- src/reference/posts_reference.ts
+++ src/reference/posts_reference.ts
@@ -1,18 +1,15 @@
 // users, posts 모듈화
 
-import { count } from "console"
-import express, { Request, Response } from "express"
+import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
 interface RequestWithConnection extends Request {
-  mysqlConnection: any
+    mysqlConnection: any
 }
 
 // 특정 유저가 소유하고 있는 게시글 전부 읽기
-router.get(
-  "/users/:userIdx/posts",
-  async (request: Request, response: Response) => {
+router.get('/users/:userIdx/posts', async (request: Request, response: Response) => {
     const req = request as RequestWithConnection
     const res = response
 
@@ -20,31 +17,28 @@ router.get(
 
     const connection = req.mysqlConnection
     const selectUsersPostsResult: {
-      title: string
-      contents: string
-      id: string
+        title: string
+        contents: string
+        id: string
     }[] = await connection.run(
-      `SELECT title, contents, u.id
+        `SELECT title, contents, u.id
     FROM posts AS p JOIN users AS u
     ON p.author_idx = u.idx
     WHERE p.author_idx = ?;`,
-      [userIdx]
+        [userIdx]
     )
 
     const responseBodyData = selectUsersPostsResult.map((data) => {
-      const { title, contents, id: userId } = data
-      return { title, contents, userId }
+        const { title, contents, id: userId } = data
+        return { title, contents, userId }
     })
     res.send(response)
-  }
-)
+})
 
 // 특정 유저의 게시글 추가
 // 이대로 적으면 문제가 하나 생긴다. 특정 유저가 없을 경우, 나중에 가입한 유저가 쓴 적도 없는 글이 유저가 작성한 것처럼 저장된다.
 // 즉 주인이 없는 게시글이 생기는 것
-router.post(
-  "/users/:userIdx/posts",
-  async (request: Request, response: Response) => {
+router.post('/users/:userIdx/posts', async (request: Request, response: Response) => {
     const req = request as RequestWithConnection
     const res = response
     const { title, contents } = req.body
@@ -53,27 +47,18 @@ router.post(
     const connection = req.mysqlConnection
 
     // users에서 해당 userIdx가 있는지 없는지 검사, 1이 나오면 user가 있는 거고 0이면 user가 없는 것
-    const countUserResult = await connection.run(
-      `SELECT COUNT(*) AS count FROM users WHERE idx = ?;`,
-      [userIdx]
-    )
+    const countUserResult = await connection.run(`SELECT COUNT(*) AS count FROM users WHERE idx = ?;`, [userIdx])
     if (countUserResult[0].count !== 1) {
-      throw new Error("해당되는 유저의 idx가 존재하지 않습니다.")
+        throw new Error('해당되는 유저의 idx가 존재하지 않습니다.')
     }
-    await connection.run(
-      `INSERT INTO posts (title, contents, author_idx ) VALUES (?, ?, ?);`,
-      [title, contents, userIdx]
-    )
+    await connection.run(`INSERT INTO posts (title, contents, author_idx ) VALUES (?, ?, ?);`, [title, contents, userIdx])
     res.send({
-      success: true,
+        success: true,
     })
-  }
-)
+})
 
 // PUT 특정 유저의 게시글에서 특정 idx의 게시글 수정
-router.put(
-  "/users/:userIdx/posts/:postIdx",
-  async (request: Request, response: Response) => {
+router.put('/users/:userIdx/posts/:postIdx', async (request: Request, response: Response) => {
     const req = request as RequestWithConnection
     const res = response
     const { userIdx, postIdx } = req.params
@@ -82,41 +67,29 @@ router.put(
     const connection = req.mysqlConnection
 
     try {
-      // throw new Error("갑작스러운 에러")
-      const countUser = await connection.run(
-        `SELECT COUNT(*) AS count FROM users WHERE idx = ?;`,
-        [userIdx]
-      )
-      if (countUser[0].count !== 1) {
-        throw new Error("해당 유저는 존재하지 않습니다.")
-      }
-      const countPost = await connection.run(
-        `SELECT COUNT(*) AS count FROM posts WHERE idx = ?;`,
-        [postIdx]
-      )
-      if (countPost[0].count !== 1) {
-        throw new Error("해당 글은 존재하지 않습니다.")
-      }
-      await connection.run(
-        `UPDATE posts SET title = ?, contents = ? WHERE idx = ?;`,
-        [title, contents, postIdx]
-      )
-      res.send({
-        success: true,
-      })
+        // throw new Error("갑작스러운 에러")
+        const countUser = await connection.run(`SELECT COUNT(*) AS count FROM users WHERE idx = ?;`, [userIdx])
+        if (countUser[0].count !== 1) {
+            throw new Error('해당 유저는 존재하지 않습니다.')
+        }
+        const countPost = await connection.run(`SELECT COUNT(*) AS count FROM posts WHERE idx = ?;`, [postIdx])
+        if (countPost[0].count !== 1) {
+            throw new Error('해당 글은 존재하지 않습니다.')
+        }
+        await connection.run(`UPDATE posts SET title = ?, contents = ? WHERE idx = ?;`, [title, contents, postIdx])
+        res.send({
+            success: true,
+        })
     } catch (e) {
-      res.send({
-        success: false,
-        errorMessage: `${e}`,
-      })
+        res.send({
+            success: false,
+            errorMessage: `${e}`,
+        })
     }
-  }
-)
+})
 
 // DELETE 특정 유저의 게시글에서 특정 idx의 게시글 삭제
-router.delete(
-  "/users/:userIdx/posts/:postIdx",
-  async (request: Request, response: Response) => {
+router.delete('/users/:userIdx/posts/:postIdx', async (request: Request, response: Response) => {
     const req = request as RequestWithConnection
     const res = response
     const { userIdx, postIdx } = req.params
@@ -124,31 +97,24 @@ router.delete(
     const connection = req.mysqlConnection
 
     try {
-      const countUser = await connection.run(
-        `SELECT COUNT(*) AS count FROM users WHERE idx = ?;`,
-        [userIdx]
-      )
-      if (countUser[0].count !== 1) {
-        throw new Error("해당 유저는 존재하지 않습니다.")
-      }
-      const countPost = await connection.run(
-        `SELECT COUNT(*) AS count FROM posts WHERE idx = ?;`,
-        [postIdx]
-      )
-      if (countPost[0].count !== 1) {
-        throw new Error("해당 글은 존재하지 않습니다.")
-      }
-      await connection.run(`DELETE FROM posts WHERE idx = ?;`, [postIdx])
-      res.send({
-        success: true,
-      })
+        const countUser = await connection.run(`SELECT COUNT(*) AS count FROM users WHERE idx = ?;`, [userIdx])
+        if (countUser[0].count !== 1) {
+            throw new Error('해당 유저는 존재하지 않습니다.')
+        }
+        const countPost = await connection.run(`SELECT COUNT(*) AS count FROM posts WHERE idx = ?;`, [postIdx])
+        if (countPost[0].count !== 1) {
+            throw new Error('해당 글은 존재하지 않습니다.')
+        }
+        await connection.run(`DELETE FROM posts WHERE idx = ?;`, [postIdx])
+        res.send({
+            success: true,
+        })
     } catch (e) {
-      res.send({
-        success: false,
-        errorMessage: `${e}`,
-      })
+        res.send({
+            success: false,
+            errorMessage: `${e}`,
+        })
     }
-  }
-)
+})
 
 export default router
